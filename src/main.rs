335ce29362a333e8c6f07370a230b47fc3f18afb
Robbie Huffman <robbie.huffman@gmail.com>
2019-03-09 10:38:04 -0500

    First output

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate tokio;
 extern crate tokio_ping;
 extern crate termion;
 
-use std::io;
+use std::io::{Write, stdout, stdin};
 
 use termion::event::Key;
 use termion::input::MouseTerminal;
@@ -14,26 +14,27 @@ use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 use termion::{color, clear, cursor};
              
-#use tui::backend::TermionBackend;
-#use tui::layout::{Constraint, Layout};
-#use tui::style::{Color, Modifier, Style};
-#use tui::widgets::{Block, Borders, Row, Table, Widget};
-#use tui::Terminal;
+//use tui::backend::TermionBackend;
+//use tui::layout::{Constraint, Layout};
+//use tui::style::{Color, Modifier, Style};
+//use tui::widgets::{Block, Borders, Row, Table, Widget};
+//use tui::Terminal;
 
-use crate::util::event::{Event, Events};
+//use crate::util::event::{Event, Events};
 
 use futures::{Future, Stream};
 
 fn main() {
-    println!("{}", clear::All);
-    println!("{}", cursor::Goto(4,4)};
-    println!("○○○○○○○○○○");
-    println!("{}", cursor::Goto(4,5)};
-    println!("○○○○○○○○○○");
-    println!("{}", cursor::Goto(4,6)};
-    println!("○○○○○○○○○○");
-    println!("{}", cursor::Goto(4,6)};
-    println!("○○○○○○○○○○");
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    writeln!(stdout,"{}", clear::All);
+    writeln!(stdout,"{}", cursor::Goto(4,4));
+    writeln!(stdout,"○○○○○○○○○○");
+    writeln!(stdout,"{}", cursor::Goto(4,5));
+    writeln!(stdout,"○○○○○○○○○○");
+    writeln!(stdout,"{}", cursor::Goto(4,6));
+    writeln!(stdout,"○○○○○○○○○○");
+    writeln!(stdout,"{}", cursor::Goto(4,6));
+    writeln!(stdout,"○○○○○○○○○○");
 
 
 }
