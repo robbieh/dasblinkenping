64499dadf5bc587c12b5ede3ea5b8259c04eff66
Robbie Huffman <robbie.huffman@gmail.com>
2019-03-08 22:06:29 -0500

    Test 1

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 
 extern crate futures;
 extern crate tokio;
-
 extern crate tokio_ping;
+extern crate termion;
 
 use std::io;
 
@@ -12,17 +12,28 @@ use termion::event::Key;
 use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
-use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Layout};
-use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, Row, Table, Widget};
-use tui::Terminal;
+use termion::{color, clear, cursor};
+             
+#use tui::backend::TermionBackend;
+#use tui::layout::{Constraint, Layout};
+#use tui::style::{Color, Modifier, Style};
+#use tui::widgets::{Block, Borders, Row, Table, Widget};
+#use tui::Terminal;
 
 use crate::util::event::{Event, Events};
 
 use futures::{Future, Stream};
 
 fn main() {
+    println!("{}", clear::All);
+    println!("{}", cursor::Goto(4,4)};
+    println!("○○○○○○○○○○");
+    println!("{}", cursor::Goto(4,5)};
+    println!("○○○○○○○○○○");
+    println!("{}", cursor::Goto(4,6)};
+    println!("○○○○○○○○○○");
+    println!("{}", cursor::Goto(4,6)};
+    println!("○○○○○○○○○○");
 
 
 }
