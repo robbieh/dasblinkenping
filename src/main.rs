d450a29d526586432e13c8da8a0e35959077c7ec
Robbie Huffman <robbie.huffman@gmail.com>
2019-03-17 23:16:19 -0400

    bug; every ping duration seems to be the same

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ extern crate futures;
 extern crate tokio;
 extern crate tokio_ping;
 extern crate termion;
+extern crate rand;
 
+use rand::Rng;
+
+use std::collections::{VecDeque};
 use std::io::{Write, stdout, stdin};
-use std::net::{IpAddr};
+use std::net::{IpAddr, Ipv4Addr};
 use std::sync::{Arc,RwLock};
+use std::thread;
+use std::thread::{sleep};
+use std::time;
 
 use termion::event::Key;
 use termion::input::MouseTerminal;
@@ -25,19 +32,52 @@ use termion::{color, clear, cursor};
 //use crate::util::event::{Event, Events};
 
 #[derive(Debug,Clone)]
-pub enum PingData {
-    Addr(IpAddr),
-    RTT(Arc<RwLock<i32>>)
+pub struct PingData {
+    addr: IpAddr,
+    rtt: Arc<RwLock<i32>>
 }
 
 use futures::{Future, Stream};
 
+//writeln!(stdout,"{}", cursor::Goto(4,8));
+//writeln!(stdout,"○○○○○○○○○○");
+fn draw(size: usize, ips: Vec<PingData>) {
+    let symbols = " +o○";
+    //let mut stdout = stdout().into_raw_mode().unwrap();
+    loop{
+        //writeln!(stdout,"{}", clear::All);
+        for n in 0..(size * size) {
+            //println!("{:#?}, {:#?}", (n % size) + 1, ( n as f32 / size as f32) as u16 + 1);
+            //writeln!(stdout,"{}", cursor::Goto((n % size) as u16 + 1, (n as f32 / size as f32) as u16 + 1) );
+            let pd = *ips[n].clone().rtt.read().unwrap();
+            println!("loop-{:?}, {:?}, {:?}", n, ips[n].addr, pd);
+            //if      pd > 750 { writeln!(stdout,"{}",  symbols.chars().nth(3).unwrap() ); } 
+            //else if pd > 500 { writeln!(stdout,"{}",  symbols.chars().nth(2).unwrap() ); }
+            //else if pd > 250 { writeln!(stdout,"{}",  symbols.chars().nth(1).unwrap() ); }
+            //else             { writeln!(stdout,"{}",  symbols.chars().nth(0).unwrap() ); }
+        }
+        thread::sleep(time::Duration::from_millis(100));
+    }
+}
+
+fn pingLoop(pd: PingData) {
+    loop {
+        let duration = rand::thread_rng().gen_range(0,1000);
+        println!("{:?}, {:?}", pd.addr, duration);
+        thread::sleep(time::Duration::from_millis(duration));
+        let mut w = pd.rtt.write().unwrap();
+        *w = duration as i32;
+    }
+}
+
 fn main() {
-    let ips: Vec<IpAddr> = Vec::new();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    writeln!(stdout,"{}", clear::All);
-    writeln!(stdout,"{}", cursor::Goto(4,8));
-    writeln!(stdout,"○○○○○○○○○○");
-
-
+    let size = 4;
+    let ips: Vec<PingData> = vec![PingData{addr: IpAddr::V4(Ipv4Addr::new(0,0,0,0)), rtt: Arc::new(RwLock::new(0))}; size * size];
+    for ip in &ips {
+        let myip = ip.clone();
+        thread::spawn(move || {
+            pingLoop(myip)
+        });
+    }
+    draw(size, ips);
 }
